66db92870dfc5a2078de225480e641a0f96c3733
bstillword <87246107+bstillword@users.noreply.github.com>
2021-09-17 16:08:08 -0700

    Updated Flix Assignment
    Completed Unit 2 assignment.

---
+++ Flix_Assign1/Flix_Assign1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix_Assign1
+//
+//  Created by Brandon Stillword on 9/16/21.
+//
+
+import UIKit
+import AlamofireImage
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
